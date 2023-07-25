85f6032572b610e72c586f864994a572a56e0dec
Praneeth Lakshman <53898584+praneeth-lakshman@users.noreply.github.com>
2023-07-25 13:46:45 +0100

    Updates

---
+++ _app/immutable/chunks/paths.1f04dd04.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1wjxt01)==null?void 0:s.base)??"/praneeth-lakshman.github.io";var t;const e=((t=globalThis.__sveltekit_1wjxt01)==null?void 0:t.assets)??a;export{e as a,a as b};
